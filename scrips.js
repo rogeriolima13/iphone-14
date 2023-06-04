e96aac74d80ed2807a07804c1e969ec5986985aa
rogeriolima13 <pistol.nation.pol@gmail.com>
2023-06-04 09:05:22 -0300

    iphone

--- scrips.js
+++ scrips.js
@@ -2,7 +2,14 @@
 
 
 function trocacor(cor){
-    let circulo = document.querySelector(".ciculo")
-    circulo.style.background= cor
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+    
 }
 
+function trocaImagem (imagem) {
+  let  imgiphone = document.querySelector (".iphone")
+  imgiphone . src  = imagem
+  console.log("agora vai")
+  
+}
